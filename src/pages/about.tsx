b5d7d335f2724809f4a0813216c23861c2c6e7ec
Sanshiroh SAKAI <sansyo@gmail.com>
2022-09-24 21:23:38 +0900

    set page meta

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,24 @@
 import type { NextPage } from 'next'
 
+import { pages } from '@/data/sheets.json'
+
 import AboutItemList from '@/components/AboutItemList'
 import AboutLead from '@/components/AboutLead'
 import CategoryMv from '@/components/CategoryMv'
 import HeadingSection from '@/components/HeadingSection'
 import MainContent from '@/components/MainContent'
+import Meta from '@/components/Meta'
 import MotionFadeInUp from '@/components/MotionFadeInUp'
 
 import DefaultLayout from '@/layout/DefaultLayout'
 
 const Home: NextPage = () => {
+  const pageMeta = pages.about
+
   return (
     <DefaultLayout>
+      <Meta {...pageMeta} />
+
       <CategoryMv category="about" />
       <MainContent>
         <MotionFadeInUp>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,25 @@
 import type { NextPage } from 'next'
 
+import { pages } from '@/data/sheets.json'
+
 import BannerInstagram from '@/components/BannerInstagram'
 import CollectionList from '@/components/CollectionList'
 import HeadingSection from '@/components/HeadingSection'
 import MainContent from '@/components/MainContent'
 import MainVisual from '@/components/MainVisual'
+import Meta from '@/components/Meta'
 import MotionFadeInUp from '@/components/MotionFadeInUp'
 import NewsList from '@/components/NewsList'
 
 import DefaultLayout from '@/layout/DefaultLayout'
 
 const Home: NextPage = () => {
+  const pageMeta = pages.top
+
   return (
     <DefaultLayout>
+      <Meta {...pageMeta} />
+
       <MainVisual />
       <MainContent>
         <MotionFadeInUp>
